34e913a0222f268b54e502c66d71eaa5f3b44739
cforamesh <cforamesh@gmail.com>
2026-02-21 12:39:14 +0530

    Task 1: Perform Basic Mathematical Operations
    Problem Statement: Write a Python program that does the following:
    1.  Takes two numbers as input from the user.
    2.  Performs the basic mathematical operations on these two numbers:
    o	Addition
    o	Subtraction
    o	Multiplication
    o	Division
    3.  Displays the results of each operation on the screen.
     Expected Output:
    The output should include the result of each operation performed

---
+++ Task1.py
@@ -0,0 +1,11 @@
+a = float(input("Enter the first number:  "))
+b = float(input("Enter the second number:  "))
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+division = a / b if b != 0 else "Undefined (division by zero)"
+print(f"Addition: {Addition}")
+print(f"Subtraction: {Subtraction}")
+print(f"Multiplication: {Multiplication}")
+print(f"Division: {division}")
+

0ca9511eb61951375c94a4e96f0afeb36ad5aeb4
cforamesh <cforamesh@gmail.com>
2026-02-21 13:07:47 +0530

    Task 2_2: Sum of Integers from 1 to 50 Using a Loop
    Problem Statement: Write a Python program that:
    1.   Uses a for loop to iterate over numbers from 1 to 50.
    2.   Calculates the sum of all integers in this range.
    3.   Displays the final sum.
     
    Expected Output:
    The program should return:
    
    The sum of numbers from 1 to 50 is : 1275

---
+++ Task 2_2.py
@@ -0,0 +1,7 @@
+#Program to calculate the sum of first 50 natural numbers using for loop statement
+
+sum=0
+for i in range(51):
+    sum = sum + i
+print(f"the sum of numbers from 1 to 50 is : {sum}")
+

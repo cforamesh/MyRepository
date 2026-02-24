fb375cd32504aecfc07bd45e75d310b0b5ee5bbe
cforamesh <cforamesh@gmail.com>
2026-02-24 19:02:23 +0530

    Task 2: Using the Math Module for Calculations
    Problem Statement: Write a Python program that:
    1.   Asks the user for a number as input.
    2.   Uses the math module to calculate the:
    o   Square root of the number
    o   Natural logarithm (log base e) of the number
    o   Sine of the number (in radians)
    3.   Displays the calculated results.
     Expected Output:
     For example, if the user enters 25, the output should be:
    
    Enter a number: 25
    Square root: 5.0
    Logarithm: 3.2188758248682006
    Sine: -0.13235175009777303

---
+++ Task_3_2.py
@@ -0,0 +1,11 @@
+import math
+
+n = int(input("Enter a number : "))
+
+sqrt = math.sqrt(n)
+loge = math.log(n)
+sin = math.sin(n)
+
+print(f"Square root : {sqrt}")
+print(f"Logarithm : {loge}")
+print(f"Sine : {sin}")
